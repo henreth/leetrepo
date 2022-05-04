cd0256a1a93924d3d67a06ba697f93c6526aef6e
Henry Miller <mail.henrym+git@gmail.com>
2022-05-04 13:31:25 -0400

    added python to singlenumber

--- 136-singlenumber/singlenumber.js
+++ 136-singlenumber/singlenumber.js
@@ -5,12 +5,18 @@
  var singleNumber = function(nums) {
     let obj = {}
     for (let i of nums){
-        if (obj[i]===undefined){
-            obj[i]= 1
-        } else {
+        if (obj[i]){
             obj[i]++
+        } else {
+            obj[i]=1
         }
     }
     return Object.keys(obj).find(key => obj[key] === 1);
-    
+
+    //alt solution for finding the key
+    //  for (number in numberCount) {
+    //     if (numberCount[number] === 1) {
+    //         return number
+    //     }
+    // }
 };
