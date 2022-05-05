522ebeeddb474380e47c8b885914f59406b8b256
Henry Miller <mail.henrym+git@gmail.com>
2022-05-04 21:58:20 -0400

    additional solutions to contains duplicate

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -10,3 +10,18 @@
     
       return Object.keys(numCount).find(key => numCount[key] > 1 ) 
 };
+
+// /**
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+//  var containsDuplicate = function(nums) {
+//   let numCount = {}
+//   for (let num of nums){
+//       if (numCount[num]){return true}
+//       else{numCount[num]=1}
+//   }
+  
+//     return false
+
+// };
