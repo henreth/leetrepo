eebff07011e5d9d0b14eb4b6f07513d8ec4f4384
Henry Miller <mail.henrym+git@gmail.com>
2022-05-04 20:18:25 -0400

    added alternative TS solution

--- 2129-capitalize-the-title/capitalize.ts
+++ 2129-capitalize-the-title/capitalize.ts
@@ -1,7 +1,10 @@
 function capitalizeTitle(title: string): string {
-    let newTitle=[]
-    title.split(' ').forEach(word=>{
-    word.length>2 ? newTitle.push(word[0].toUpperCase() + word.toLowerCase().slice(1,)) : newTitle.push(word.toLowerCase())
-        })
+    let newTitle = []
+    title.split(' ').forEach(word => {
+        word.length > 2 ? newTitle.push(word[0].toUpperCase() + word.toLowerCase().slice(1,)) : newTitle.push(word.toLowerCase())
+    })
     return newTitle.join(' ')
+
+    // alternative TS solution
+    // return title.toLowerCase().split(' ').map(word=>word.length>2 ? word[0].toUpperCase() + word.slice(1,) : word).join(' ')
 };
