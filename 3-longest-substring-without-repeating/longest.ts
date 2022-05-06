047e769720d6738eddf85728238263da353cc41e
Henry Miller <mail.henrym+git@gmail.com>
2022-05-05 21:22:22 -0400

    finished ts solution for longest substring without repeating

---
+++ 3-longest-substring-without-repeating/longest.ts
@@ -0,0 +1,23 @@
+function lengthOfLongestSubstring(s: string): number {
+    let sub = ''
+    let longest = ''
+    for (let char of s){
+        if (sub.split('').includes(char)){
+            if (longest.length < sub.length ){
+                longest = sub
+            }
+            if (longest.indexOf(char)- longest.indexOf(char,longest.indexOf(char))===1){
+                sub = char
+            } else {
+                sub = sub.slice(sub.indexOf(char)+1,) + char
+            }
+
+        } else {
+            sub+=char
+            if (longest.length < sub.length){
+                longest=sub
+            }
+        }
+    }
+    return longest.length
+};
