189adf3710c7927ab847e96c4e8b573d68deba30
Henry Miller <mail.henrym+git@gmail.com>
2022-05-05 23:01:51 -0400

    added python to 274

--- 274-h-index/hindex.js
+++ 274-h-index/hindex.js
@@ -5,7 +5,7 @@
  var hIndex = function(citations) {
     citations.sort((a,b)=>b-a)
     for(i=0;i<citations.length;i++){
-        console.log(citations[i],i+1)
+
         if(citations[i]<i+1) return i
     }
     return i
