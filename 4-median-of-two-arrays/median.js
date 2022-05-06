90682d1e3481052191cba907cd8ab51a4040c672
Henry Miller <mail.henrym+git@gmail.com>
2022-05-06 14:24:43 -0400

    added js to 4

---
+++ 4-median-of-two-arrays/median.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let nums = [].concat(nums1,nums2).sort((num1,num2)=>num1-num2)
+    let medIdx = Math.round(nums.length/2) - 1 
+    if (nums.length %2!=0){
+        return nums[medIdx]
+    } else {
+        let idx = Math.round(nums.length/2)
+        return (nums[medIdx] + nums[idx])/2
+    }
+
+};
