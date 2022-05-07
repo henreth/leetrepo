259be92ffab500eeb3c881b5c0a30eb268f652b0
Henry Miller <mail.henrym+git@gmail.com>
2022-05-06 20:40:04 -0400

    updated 1 js

--- 1-twosum/twosum.js
+++ 1-twosum/twosum.js
@@ -20,3 +20,18 @@
      }
      return []
  };
+
+//   const twoSum = function (nums, target) {
+//     // new Map() creates an object that holds key-value pairs and remembers what order they were inserted in
+//      const map = new Map();
+//      for (let i = 0; i < nums.length; i++) {
+//        const diff = target - nums[i];
+//        if (map.has(diff)) {
+//          // map.has will tell you if diff is currently in the map
+//          return [map.get(diff), i];
+//          // map.get will return the value associated with the key of diff
+//        }
+//        map.set(nums[i], i);
+//        // map.set will create a key in map with the value of i (the index in the array)
+//      }
+//    };
