da726a295ec29ae5f0665dd2a1e2a4d82bfa71c0
Henry Miller <mail.henrym+git@gmail.com>
2022-05-06 22:32:49 -0400

    finished 268 ts

--- 268-missing-number/missing.ts
+++ 268-missing-number/missing.ts
@@ -6,3 +6,11 @@ function missingNumber(nums: number[]): number {
 
   return sum;
 }
+
+// function missingNumber(nums: number[]): number {
+//     nums.sort((a,b) => a-b);
+//     for(let i=0 ; i<nums.length; i++){
+//         if(i !== nums[i]) return i;
+//     }
+//     return nums[nums.length-1]+1
+// };
