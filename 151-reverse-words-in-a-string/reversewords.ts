3b399ae24abe189eab7f5b833dc0aee365edf998
Henry Miller <mail.henrym+git@gmail.com>
2022-05-06 22:39:36 -0400

    updated 151 ts

--- 151-reverse-words-in-a-string/reversewords.ts
+++ 151-reverse-words-in-a-string/reversewords.ts
@@ -1,5 +1,7 @@
 function reverseWords(s: string): string {
     let words = s.match(/[^ ]+/g)
+    // .match searches the string for the input and returns that input if so
+    // /g searches the string globally and returns the input the number of times the input appears 
     return words.reverse().join(' ')
 }
 
