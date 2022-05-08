3f3188ce6616b487e32697b772c401819c50d91e
Henry Miller <mail.henrym+git@gmail.com>
2022-05-08 16:01:57 -0400

    finished 26 rs

---
+++ 26-remove-duplicates-from-sorted-array/remove.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        match nums.is_empty() {
+            true => 0,
+            false => {
+                let mut prev = 0;
+                for i in 1..nums.len() {
+                    if nums[prev] != nums[i] {
+                        prev += 1;
+                        nums[prev] = nums[i];
+                    }
+                }
+                (prev + 1) as i32
+            }
+        }
+    }
+}
