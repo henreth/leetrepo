ad208ccd3ec6ded23a6d90c28f2e111eebc8486c
Henry Miller <mail.henrym+git@gmail.com>
2022-05-08 16:12:03 -0400

    finished 412 ts

--- 412-fizzbuzz/fizzbuzz.ts
+++ 412-fizzbuzz/fizzbuzz.ts
@@ -1,4 +1,6 @@
 function fizzBuzz(n: number): string[] {
+    // [..Array(n).keys()] creates an array of a range from 0 to n
+    // .keys is important to create the values
     return [...Array(n).keys()].map((i) => {
       const num = i + 1;
       if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
