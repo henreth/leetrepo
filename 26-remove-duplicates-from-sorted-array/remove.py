1e71b9f863208f249123755fa97384dd7f1bffc7
Henry Miller <mail.henrym+git@gmail.com>
2022-05-08 15:49:13 -0400

    finished 26 py

---
+++ 26-remove-duplicates-from-sorted-array/remove.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+    # non destructive function that returns the sorted unique values in the beginning of the array with the remaining original values at the end 
+    # create counter that is also used for indexing the unique values
+        x = 1
+        for i in range(len(nums)-1):
+            if(nums[i]!=nums[i+1]):
+                # if the value does not equal the next value in the array
+                # move the value to the front of the array at the index of the current counter
+                nums[x] = nums[i+1]
+                # increase the counter
+                x+=1
+        # the counter will return the same value as the length of the unique values 
+        return(x)
