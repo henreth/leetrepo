ce95cb0a484a988f255d96ceacedc5c9663998dc
Henry Miller <mail.henrym+git@gmail.com>
2022-05-08 15:33:43 -0400

    updated 26 js

--- 26-remove-duplicates-from-sorted-array/remove.js
+++ 26-remove-duplicates-from-sorted-array/remove.js
@@ -4,5 +4,8 @@
  */
  var removeDuplicates = function(nums) {
     nums.splice(0, nums.length, ...(new Set(nums)));
+    // third and on parameters are added to the array beginning from the start value in splices
     // ...(new Set(nums)) -> copies in only the unique values of nums
+    // set objects are collections of vlaues that stores unique values of any type
+    // all values have to be distinct 
 };
