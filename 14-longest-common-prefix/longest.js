e3fad36a08992fe451877b6c36d3610f7dab5d0f
Henry Miller <mail.henrym+git@gmail.com>
2022-05-09 21:39:37 -0400

    finished 14 js

--- 14-longest-common-prefix/longest.js
+++ 14-longest-common-prefix/longest.js
@@ -2,6 +2,28 @@
  * @param {string[]} strs
  * @return {string}
  */
- var longestCommonPrefix = function(strs) {
-    
+var longestCommonPrefix = function (strs) {
+    if (strs.length === 0) return '';
+    const first = strs[0];
+
+    for (let i = 0; i < first.length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            const other = strs[j];
+            if (other[i] !== first[i]) {
+                return other.slice(0, i);
+            }
+        }
+    }
+
+    return first;
+
 };
+
+// function longestCommonPrefix (strs) {
+//     let ans = "";
+//     for (i in strs[0]) {
+//       if (!strs.every((el) => el[i] === strs[0][i])) { break; }
+//       ans += strs[0][i];
+//     }
+//     return ans;
+//   };
