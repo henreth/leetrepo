8826b9af162168269c1097beb8a129457bbbbabd
Henry Miller <mail.henrym+git@gmail.com>
2022-05-12 17:17:17 -0400

    finished 20 ts

---
+++ 20-valid-parentheses/valid.ts
@@ -0,0 +1,20 @@
+function isValid(s: string): boolean {
+    if (s.length % 2 !=0) return false
+    const stack = [];
+    const map = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+    }
+    
+    for (let i = 0 ; i < s.length ; i++) {
+        let c = s[i];
+        if (map[c]) {
+          stack.push(map[c])
+        } else if (c !== stack.pop()) {
+          return false;
+        } 
+    }
+    
+    return !stack.length;
+};
