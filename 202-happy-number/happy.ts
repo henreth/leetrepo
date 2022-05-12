4ee037e35a008f1d914227cfa6e26485f0772187
Henry Miller <mail.henrym+git@gmail.com>
2022-05-11 22:53:41 -0400

    finished 202 ts

---
+++ 202-happy-number/happy.ts
@@ -0,0 +1,14 @@
+function isHappy(n: number): boolean {
+    var seen = {};
+    while (n !== 1 && !seen[n]) {
+        seen[n] = true;
+        n = sumOfSquares(n);
+    }
+    return n === 1 
+};
+
+function sumOfSquares(numString) {
+    return numString.toString().split('').reduce(function(sum, num) {
+        return sum + Math.pow(num, 2);
+    }, 0);
+}
