65f7ebb33d107ee0ef2ee223a22d3e8bf7d6b36f
Henry Miller <mail.henrym+git@gmail.com>
2022-05-12 17:23:31 -0400

    finished 20 js

--- 20-valid-parentheses/valid.js
+++ 20-valid-parentheses/valid.js
@@ -3,22 +3,22 @@
  * @return {boolean}
  */
  var isValid = function(s) {
-    if (s.length % 2 !=0) return false
+    if (s.length % 2 !=0) return false // if the string has an odd length, then there is an unmatched bracket/parantheses
     const stack = [];
-    const map = {
+    const map = { // dictionary for brackets that corresponds opening brackets for keys and their matching closing brackets for values
       '(': ')',
       '[': ']',
       '{': '}'
     }
     
-    for (let i = 0 ; i < s.length ; i++) {
+    for (let i = 0 ; i < s.length ; i++) { //iterate through the string 
         let c = s[i];
-        if (map[c]) {
-          stack.push(map[c])
-        } else if (c !== stack.pop()) {
+        if (map[c]) { // if the current character is an opening... 
+          stack.push(map[c])  // add the closing bracket to the stack
+        } else if (c !== stack.pop()) { // if the last value is not the same type of the current value, return false
           return false;
         } 
     }
     
-    return !stack.length;
+    return !stack.length; // if the stack has a length of 0, then all the brackets will have been matched
 };
