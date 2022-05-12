dabbe1012791e7643c433fe82179515e617145a4
Henry Miller <mail.henrym+git@gmail.com>
2022-05-12 17:11:40 -0400

    finished 20 js

---
+++ 20-valid-parentheses/valid.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+    if (s.length % 2 !=0) return false
+    const stack = [];
+    const map = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+    }
+    
+    for (let i = 0 ; i < s.length ; i++) {
+        let c = s[i];
+        if (map[c]) {
+          stack.push(map[c])
+        } else if (c !== stack.pop()) {
+          return false;
+        } 
+    }
+    
+    return !stack.length;
+};
