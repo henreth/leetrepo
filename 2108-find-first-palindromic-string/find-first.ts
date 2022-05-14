1633644770fb181505d73912e671803ee7379fc2
Henry Miller <mail.henrym+git@gmail.com>
2022-05-14 01:18:18 -0400

    updated 2108 ts

--- 2108-find-first-palindromic-string/find-first.ts
+++ 2108-find-first-palindromic-string/find-first.ts
@@ -2,9 +2,8 @@ function firstPalindrome(words: string[]): string {
     for (let s of words) {
         let input = s.replace(/[^0-9a-z]/gi, '').toLowerCase()
         let reverse = input.split('').reverse().join('')
-        if (input === reverse ){
-            return s
-        }
+        if (input === reverse ) return s
+    
     }
 
     return ''
