aba777ed0225589ac294799c4f4ffdbc0e96dcde
Henry Miller <mail.henrym+git@gmail.com>
2022-05-14 15:04:33 -0400

    updated 29 py

--- 29-divide-two-integers/divide.py
+++ 29-divide-two-integers/divide.py
@@ -1,20 +1,20 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        negative = ((dividend<0 and divisor >0) or (dividend>0 and divisor <0));
-        ret = 0;       
-        dividend,divisor=abs(dividend),abs(divisor);
-        count,sub = 1, divisor;
+        negative = ((dividend<0 and divisor >0) or (dividend>0 and divisor <0))
+        ret = 0       
+        dividend,divisor=abs(dividend),abs(divisor)
+        count,sub = 1, divisor
 
         while(dividend >= divisor):
             if(dividend>=sub):
-                dividend-=sub;
-                ret+=count;
-                sub=(sub<<1);
-                count=(count<<1);
+                dividend-=sub
+                ret+=count
+                sub=(sub<<1)
+                count=(count<<1)
             else:
-                sub=(sub>>1);
-                count=(count>>1);
+                sub=(sub>>1)
+                count=(count>>1)
 
         if(negative):
-            ret=-ret;
-        return min(max(-2147483648,ret),2147483647);
+            ret=-ret
+        return min(max(-2147483648,ret),2147483647)
