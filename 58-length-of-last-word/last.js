2be65c73eb6b1ea47709bf6ad6e3a5dc2e1d77d4
Henry Miller <mail.henrym+git@gmail.com>
2022-05-14 00:46:50 -0400

    updated 58 js

--- 58-length-of-last-word/last.js
+++ 58-length-of-last-word/last.js
@@ -3,6 +3,14 @@
  * @return {number}
  */
  var lengthOfLastWord = function(s) {
-    s = s.split(' ').filter(x=>x!='')
-    return s[s.length-1].length
+    return s.trim().split(' ').pop().length
 };
+
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+//  var lengthOfLastWord = function(s) {
+//     s = s.split(' ').filter(x=>x!='')
+//     return s[s.length-1].length
+// };
