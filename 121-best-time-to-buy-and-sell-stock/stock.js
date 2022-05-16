ba89425e1d392a63fcdd4d351a73ef83d4407fbb
Henry Miller <mail.henrym+git@gmail.com>
2022-05-15 22:15:52 -0400

    finished 121 js

---
+++ 121-best-time-to-buy-and-sell-stock/stock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+        let left = 0 
+        let right = 1 
+        let max_profit = 0
+        while (right < prices.length) {
+            let currentProfit = prices[right] - prices[left] 
+            if (prices[left] < prices[right]){ 
+                max_profit = Math.max(currentProfit,max_profit) 
+            }
+            else{
+                left = right} 
+            right += 1
+        }
+        return max_profit
+};
