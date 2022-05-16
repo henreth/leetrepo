cfcffb4c263e7fe58d4da5376c708df22f0dd8cd
Henry Miller <mail.henrym+git@gmail.com>
2022-05-16 19:56:00 -0400

    updated 283 py

--- 283-move-zeroes/move.py
+++ 283-move-zeroes/move.py
@@ -3,12 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nonZeroes = 0 
+        nonZeroes=0
         for i in range(len(nums)):
-            if nums[i]!=0:
-                nums[nonZeroes] = nums[i]
-                nonZeroes+=1
+            if nums[i] !=0:
+                nums[nonZeroes], nums[i] = nums[i], nums[nonZeroes] # swap positions if nums[i] != 0 
+                nonZeroes +=1
 
-        for i in range(nonZeroes,len(nums)):
-            nums[i] = 0
+# first solution: less optimized
+    # def moveZeroes(self, nums: List[int]) -> None:
+    #     """
+    #     Do not return anything, modify nums in-place instead.
+    #     """
+    #     nonZeroes = 0 
+    #     for i in range(len(nums)):
+    #         if nums[i]!=0:
+    #             nums[nonZeroes] = nums[i]
+    #             nonZeroes+=1
+
+    #     for i in range(nonZeroes,len(nums)):
+    #         nums[i] = 0
         
