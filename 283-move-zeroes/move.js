cc837514e080f300bfa895ee08e01a181311c0ac
Henry Miller <mail.henrym+git@gmail.com>
2022-05-16 20:01:18 -0400

    updated 283 js

--- 283-move-zeroes/move.js
+++ 283-move-zeroes/move.js
@@ -2,16 +2,30 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+
  var moveZeroes = function(nums) {
     let nonZeroes = 0 
     for (let i = 0; i < nums.length; i++){
-        if (nums[i]!=0){
-            nums[nonZeroes] = nums[i]
+        let val = nums[i]
+        nums[i] = 0 
+        if (val!=0){
+            nums[nonZeroes] = val
             nonZeroes++
         }
-    }
-    
-    for (let i =nonZeroes;i < nums.length;i++ ){
-        nums[i] = 0
-    }
+    }  
 };
+
+// original solution:
+//  var moveZeroes = function(nums) {
+//     let nonZeroes = 0 
+//     for (let i = 0; i < nums.length; i++){
+//         if (nums[i]!=0){
+//             nums[nonZeroes] = nums[i]
+//             nonZeroes++
+//         }
+//     }
+    
+//     for (let i =nonZeroes;i < nums.length;i++ ){
+//         nums[i] = 0
+//     }
+// };
