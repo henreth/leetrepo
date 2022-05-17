dbda3a2fc259c7c1b87167ad19c4a934e01aad32
Henry Miller <mail.henrym+git@gmail.com>
2022-05-16 20:00:29 -0400

    updated 283 ts

--- 283-move-zeroes/move.ts
+++ 283-move-zeroes/move.ts
@@ -4,13 +4,26 @@
 function moveZeroes(nums: number[]): void {
     let nonZeroes = 0 
     for (let i = 0; i < nums.length; i++){
-        if (nums[i]!=0){
-            nums[nonZeroes] = nums[i]
+        let val = nums[i]
+        nums[i] = 0 
+        if (val!=0){
+            nums[nonZeroes] = val
             nonZeroes++
         }
     }
-    
-    for (let i =nonZeroes;i < nums.length;i++ ){
-        nums[i] = 0
-    }
 };
+
+// original solution: 
+// function moveZeroes(nums: number[]): void {
+//     let nonZeroes = 0 
+//     for (let i = 0; i < nums.length; i++){
+//         if (nums[i]!=0){
+//             nums[nonZeroes] = nums[i]
+//             nonZeroes++
+//         }
+//     }
+    
+//     for (let i =nonZeroes;i < nums.length;i++ ){
+//         nums[i] = 0
+//     }
+// };
