5a54cc74457fa5fe260dce053c241a0c938aa2ca
Henry Miller <mail.henrym+git@gmail.com>
2022-05-17 23:50:01 -0400

    updated 168 py

--- 168-excel-sheet-column-title/excel.py
+++ 168-excel-sheet-column-title/excel.py
@@ -2,7 +2,13 @@ class Solution:
     def convertToTitle(self, n: int) -> str:
         res = ""
         while n:
-            res = chr(ord("A") + (n - 1) % 26) + res
+            res = chr(ord("A") + (n - 1) % 26) + res 
+            # chr(int) finds the unicode character at the specified index
+            # ord(char) finds the unicode location of the specified character
+            # we add ord("A") to start at the alphabet
+            # we subtract n - 1 because the index starts at 0 so the alphabet is actually 0-25
+            # (n-1) % 26 will find how many indexes after A the intended character will be
             n = (n - 1) // 26
+            # alter n to find the potential next character
         
         return res
