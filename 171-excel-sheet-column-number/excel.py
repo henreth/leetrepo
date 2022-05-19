3815b3dbb361327abddea065ce4750e19fdb188b
Henry Miller <mail.henrym+git@gmail.com>
2022-05-19 00:29:17 -0400

    finished 171 py

---
+++ 171-excel-sheet-column-number/excel.py
@@ -0,0 +1,7 @@
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+          res = 0
+          for i in range(len(s)):
+            res += (ord(s[i]) - 64) * 26 ** (len(s) - i - 1)
+            
+          return res
