be69dad90827cffc7837c0932000f2c29279f529
Henry Miller <mail.henrym+git@gmail.com>
2022-05-19 00:40:02 -0400

    finished 2194 js

---
+++ 2194-cells-in-a-range/range.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var cellsInRange = function(s) {
+    let rows = s.split(':')
+    let startLetter = rows[0].charCodeAt(0)
+    let endLetter = rows[1].charCodeAt(0)
+    
+    let res = []
+    for (let j = Number(startLetter); j <= Number(endLetter); j++){
+        let letter = String.fromCharCode(j)
+        for (let i = Number(rows[0][1]); i <= Number(rows[1][1]); i++ ){
+                res.push(letter+i)
+        }
+    }
+
+    return res
+};
