e7e7aa9a97289295c524451ce90467657ea49844
Henry Miller <mail.henrym+git@gmail.com>
2022-05-19 00:16:58 -0400

    updated 171 ts

--- 171-excel-sheet-column-number/execl.ts
+++ 171-excel-sheet-column-number/execl.ts
@@ -1,10 +1,11 @@
 function titleToNumber(s: string): number {
     let res = 0;
     for (let i = 0; i < s.length; i++) {
-      console.log(s.charCodeAt(i), 26 ** (s.length - i - 1))
+      // s.charCodeAt(i) - 64, finds the unicode value of the string. has to reduce by 64 to find the lowercase value 
+      // as that starts at 1 instead of the upper case
+      // * 26 ** (s.length - i -1) determines the position of the current character
+      // ie: if its just A or AA, etc
       res += (s.charCodeAt(i) - 64) * 26 ** (s.length - i - 1);  
-        console.log(res)
     }
-      console.log(' ')
     return res;
   };
