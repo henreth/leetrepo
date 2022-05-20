16131acd69e2a8406bd07d458c8ff0faf61dbc77
Henry Miller <mail.henrym+git@gmail.com>
2022-05-20 09:08:48 -0400

    updated 1672 ts

--- 1672-richest-customer-wealth/richest.ts
+++ 1672-richest-customer-wealth/richest.ts
@@ -1,12 +1,21 @@
 function maximumWealth(accounts: number[][]): number {
     let max = 0;
     for (let customer of accounts){
-        let sum = 0
-        for (let account of customer){
-            sum += account        
-        }
-        max = Math.max(max,sum)
+        max = Math.max(max,customer.reduce((tot,val)=>tot+val))
     }
-    
     return max
 };
+
+// Non-reduce function:
+// function maximumWealth(accounts: number[][]): number {
+//     let max = 0;
+//     for (let customer of accounts){
+//         let sum = 0
+//         for (let account of customer){
+//             sum += account        
+//         }
+//         max = Math.max(max,sum)
+//     }
+    
+//     return max
+// };
