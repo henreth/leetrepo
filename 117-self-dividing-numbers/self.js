1215bb9ccc9b40ea20cbaa42f39d9277c719c4e7
Henry Miller <mail.henrym+git@gmail.com>
2022-05-20 23:51:53 -0400

    finished 117 js

---
+++ 117-self-dividing-numbers/self.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+ var selfDividingNumbers = function(left, right) {
+    let nums = []
+    for (let i = left; i <= right; i++){
+        if (i.toString().split('').every( num=> i % parseInt(num) === 0 )) nums.push(i)
+    }
+    return nums
+};
