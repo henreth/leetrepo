38f178363227e55b7f773ba59134df71f34672e1
Henry Miller <mail.henrym+git@gmail.com>
2022-05-22 14:55:18 -0400

    u 1528 js

--- 1528-shuffle-string/shuffle.js
+++ 1528-shuffle-string/shuffle.js
@@ -4,9 +4,17 @@
  * @return {string}
  */
  var restoreString = function(s, indices) {
-    let str = new Array(indices.length);   
-    for(let i=0; i<indices.length;i++){
-        str[indices[i]] = s[i];
-    }
-    return str.join("");  
+    let str = [...Array(indices.length).keys()]
+    str.forEach((num,i)=>str[indices[i]] = s[i])
+    return str.join('')
 };
+
+
+// Simpler solution:
+//  var restoreString = function(s, indices) {
+//     let str = new Array(indices.length);   
+//     for(let i=0; i<indices.length;i++){
+//         str[indices[i]] = s[i];
+//     }
+//     return str.join("");  
+// };
