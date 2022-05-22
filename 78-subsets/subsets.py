fdbd9a55a6c23d4a7f7a756dd7891fc518b74550
Henry Miller <mail.henrym+git@gmail.com>
2022-05-21 23:34:45 -0400

    f 78 py

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,6 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for num in sorted(nums):
+            res += [item+[num] for item in res]
+        return res
