edb6d77ea737e528fc1f41d782ff2db39e844fa0
Henry Miller <mail.henrym+git@gmail.com>
2022-05-22 23:43:54 -0400

    f 647 ts

---
+++ 647-palindromic-substrings/palindromic.ts
@@ -0,0 +1,17 @@
+function countSubstrings(s: string): number {
+        let ans = 0
+        for (let i=0; i<s.length; i++){
+            for (let j=0; j < 2; j++){
+                let left = i
+                let right = left + j
+
+                while ((left >= 0) && (right < s.length) && (s[left] == s[right])){
+                    ans += 1
+                    left -= 1
+                    right += 1
+                }
+            }
+        }
+        return ans
+    
+};
