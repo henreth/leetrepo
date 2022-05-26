680d0bff07412dda0fd054245b621ffb7cef804b
Henry Miller <mail.henrym+git@gmail.com>
2022-05-26 14:27:06 -0400

    u 1 js

--- 1-twosum/twosum.js
+++ 1-twosum/twosum.js
@@ -6,14 +6,16 @@
  var twoSum = function(nums, target) {
     let targetMinusNum = {}
      for (let num of nums) {
-         if (target - num in targetMinusNum) {
-             if (nums.indexOf(target - num) === nums.indexOf(num)) {
+         if (target - num in targetMinusNum) { // if target - num is in the obj, that means there is an element in nums that adds with the current num to our target
+             if (nums.indexOf(target - num) === nums.indexOf(num)) { // account for if the value of the two elements are the same
                  return [nums.indexOf(target - num), nums.indexOf(num, nums.indexOf(num) + 1)]
-                 // nums.indexOf(num,nums.indexOf(num) + 1 ) 
-                //   will find the index of the value (num) after the position of nums.indexOf(num) + 1
-              } else {
+                  // nums.indexOf(num,nums.indexOf(num) + 1 ) -> will find the index of the value (num) after the position of nums.indexOf(num) + 1
+                 // if target-num === num; nums.indexOf(target - num) === nums.indexOf(num) 
+             } else {
                  return [nums.indexOf(target - num), nums.indexOf(num)]
              }
+             // write the above if else statement as a one line ternary
+            //  return nums.indexOf(target - num) === nums.indexOf(num) ? [nums.indexOf(target - num), nums.indexOf(num, nums.indexOf(num) + 1)] : [nums.indexOf(target - num), nums.indexOf(num)]
          } else {
              targetMinusNum[num] = true
          }
--- 26-remove-duplicates-from-sorted-array/remove.ts
+++ 26-remove-duplicates-from-sorted-array/remove.ts
@@ -1,8 +1,5 @@
 function removeDuplicates(nums: number[]): number {
   for (let i = 1; i < nums.length; i++) {
-      // for sorted arrays only:
-    // if (nums[i] === nums[i - 1]) {
-        // for sorted and non-sorted arrays
     if (nums.indexOf(nums[i]) != i) {
         // if value at i is in the sorted array
         // remove the value at the current index
