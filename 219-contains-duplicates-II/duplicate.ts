c6fe86e224f903f0cd27b56c70106269d6b5cf44
Henry Miller <mail.henrym+git@gmail.com>
2022-06-07 10:12:39 -0400

    f 219 ts

---
+++ 219-contains-duplicates-II/duplicate.ts
@@ -0,0 +1,10 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (i - map.get(nums[i]) <= k) {
+      return true;
+    }
+    map.set(nums[i], i);
+  }
+  return false;
+};
